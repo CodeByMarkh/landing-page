598cd6250720c951e377835a6a57590dde03fa12
CodeByMarkh <markmwangi165@gmail.com>
2024-07-12 21:43:22 +0300

    finished

---
+++ script.js
@@ -0,0 +1,7 @@
+function imgSlider(image) {
+    document.querySelector('.car').src = 'images/' + image;
+}
+
+function changeCircleColor(color) {
+    document.querySelector('.circle').style.background = color;
+}
